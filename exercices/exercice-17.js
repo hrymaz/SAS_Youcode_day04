b80414a2eaba5bcb33a4ae4227ea00f3213639ad
RH <rh@HrymazMohamed.local>
2026-09-14 13:49:13 +0100

    code 7

--- exercices/exercice-17.js
+++ exercices/exercice-17.js
@@ -14,16 +14,3 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-function fruit(arr){
-let count = {};
-for(let i = 0; i < arr.length ;i++ ){
-    if(count[arr[i]]){
-    count[arr[i]]++;
-    }else {
-        count[arr[i]]=1;
-    }
-  }
-  return count ;
-}
-let arr = ["pomme", "banane", "pomme", "orange", "banane", "pomme"];
-console.log(fruit(arr));
